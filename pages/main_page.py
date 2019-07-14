4e3667a1ac2d5b1df475e4e116aa5092433fc885
Marat <kashaevmt@gmail.com>
2019-07-14 14:59:06 +0300

    check login link

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,3 +5,6 @@ from selenium.webdriver.common.by import By
 class MainPage(BasePage):
   def go_to_login_page(self):
     self.browser.find_element(By.CSS_SELECTOR, "#login_link").click()
+
+  def should_be_login_link(self):
+    self.browser.find_element(By.CSS_SELECTOR, "#login_link")
